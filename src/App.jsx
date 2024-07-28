6b8f2f45624fbe45200bd61b416ac34a863581c8
nanakwametuffour <nanakwametuffour44@gmail.com>
2024-07-28 13:20:58 -0700

    create content component and show featured and highlights

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import Hero from './components/Hero'
- import { heroapi, popularsales, toprateslaes } from './data/data'
+ import { heroapi, popularsales, toprateslaes, highlight, sneaker } from './data/data'
 import Product from './components/Product'
+import Content from './components/Content';
 export default function App() {
   return (
     <>
-      <main className='flex flex-col gap-16'>
+      <main className="flex flex-col gap-16">
         <Hero heroapi={heroapi} />
-        <Product endpoint={popularsales} ifExists/>
-        <Product endpoint={toprateslaes}/>
+        <Product endpoint={popularsales} ifExists />
+        <Content endpoint={highlight} ifExists/>
+        <Product endpoint={toprateslaes} />
+        <Content endpoint={sneaker} />
       </main>
     </>
   );
